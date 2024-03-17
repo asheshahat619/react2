1fbef8397c537cf60c3d0480a4532643d18840a1
Ashish Bhat <asheshabhat@gmail.com>
2024-03-17 15:51:06 +0530

    second commit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const heading = React.createElement("h1", {}, "this is h1")
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+import React from "react";
+import ReactDOM from "react-dom";
+const jsxHeading = < h1 className = "jsxHeadingclass" > This is Jsx heading < / h1 >
+const HeadingComponent = () => < h1 > This is heading component < /h1>
+const root = ReactDOM.createRoot(document.getElementById("root")) { /* root.render([heading, jsxHeading]) */ }
+root.render( < HeadingComponent / > )
